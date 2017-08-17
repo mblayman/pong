38b09b7650349cc6c9329e4cde473e67f2324f22
Matt Layman <matthewlayman@gmail.com>
2017-08-16 23:10:41 -0400

    Draw the ball.

--- pong/entities/ball.lua
+++ pong/entities/ball.lua
@@ -2,16 +2,24 @@
 local Ball = {}
 Ball.__index = Ball
 
+local RADIUS = 32
+
 local function construct()
   local self = setmetatable({}, Ball)
+
+  -- Start the ball in the middle of the court.
+  self.x = love.graphics.getWidth() / 2
+  self.y = love.graphics.getHeight() / 2
+
   return self
 end
 setmetatable(Ball, {__call = construct})
 
-function Ball:update(dt)
+function Ball:update(dt, key_state)
 end
 
 function Ball:draw()
+  love.graphics.circle('fill', self.x, self.y, RADIUS)
 end
 
 return Ball
