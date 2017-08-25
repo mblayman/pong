4ea55b82480ebdcb41c491ada1d1b778a8c4bb2a
Matt Layman <matthewlayman@gmail.com>
2017-08-24 23:43:33 -0400

    Move all the gameplay to an Ingame gamestate.

---
+++ pong/gamestates/ingame.lua
@@ -0,0 +1,59 @@
+local Ingame = {}
+
+local Goal = require 'pong.goal'
+local KeyState = require 'pong.key_state'
+local Scene = require 'pong.scene'
+local Ball = require 'pong.entities.ball'
+local Paddle = require 'pong.entities.paddle'
+
+local entities = {}
+local key_state = KeyState()
+local scene = Scene()
+
+-- Make the left and right goals.
+local function make_goals()
+  local height = love.graphics.getHeight()
+  local right_goal_x = love.graphics.getWidth() - Goal.WIDTH
+  return Goal(0, 0, height), Goal(right_goal_x, 0, height)
+end
+
+-- Make the left and right paddles.
+local function make_paddles()
+  local paddle_y = love.graphics.getHeight() / 2 - Paddle.HEIGHT / 2
+  local right_paddle_x = love.graphics.getWidth() - Paddle.WIDTH
+  return Paddle(0, paddle_y, 'left_player_up', 'left_player_down'),
+         Paddle(right_paddle_x, paddle_y, 'right_player_up', 'right_player_down')
+end
+
+function Ingame:init()
+  left_goal, right_goal = make_goals()
+  scene:add_goal(left_goal)
+  scene:add_goal(right_goal)
+
+  entities.left_paddle, entities.right_paddle = make_paddles()
+  scene:add_paddle(entities.left_paddle)
+  scene:add_paddle(entities.right_paddle)
+
+  entities.ball = Ball(scene)
+end
+
+function Ingame:update(dt)
+  key_state:update()
+
+  for i, entity in pairs(entities) do
+    entity:update(dt, key_state)
+  end
+end
+
+function Ingame:draw()
+  -- Draw the background.
+  local mid_x = love.graphics.getWidth() / 2
+  love.graphics.line(mid_x, 0, mid_x, love.graphics.getHeight())
+
+  -- Draw the moving stuff.
+  for i, entity in pairs(entities) do
+    entity:draw()
+  end
+end
+
+return Ingame
--- pong/main.lua
+++ pong/main.lua
@@ -1,12 +1,6 @@
-local Goal = require 'pong.goal'
-local KeyState = require 'pong.key_state'
-local Scene = require 'pong.scene'
-local Ball = require 'pong.entities.ball'
-local Paddle = require 'pong.entities.paddle'
+local Gamestate = require 'pong.vendor.hump.gamestate'
 
-local entities = {}
-local key_state = KeyState()
-local scene = Scene()
+local Ingame = require 'pong.gamestates.ingame'
 
 -- Lua's random number generator doesn't have enough entropy to start.
 -- Without some initial calls, the first random is always the same.
@@ -17,52 +11,8 @@ local function init_random()
   math.random()
 end
 
--- Make the left and right goals.
-local function make_goals()
-  local height = love.graphics.getHeight()
-  local right_goal_x = love.graphics.getWidth() - Goal.WIDTH
-  return Goal(0, 0, height), Goal(right_goal_x, 0, height)
-end
-
--- Make the left and right paddles.
-local function make_paddles()
-  local paddle_y = love.graphics.getHeight() / 2 - Paddle.HEIGHT / 2
-  local right_paddle_x = love.graphics.getWidth() - Paddle.WIDTH
-  return Paddle(0, paddle_y, 'left_player_up', 'left_player_down'),
-         Paddle(right_paddle_x, paddle_y, 'right_player_up', 'right_player_down')
-end
-
 function love.load()
   init_random()
-
-  left_goal, right_goal = make_goals()
-  scene:add_goal(left_goal)
-  scene:add_goal(right_goal)
-
-  entities.left_paddle, entities.right_paddle = make_paddles()
-  scene:add_paddle(entities.left_paddle)
-  scene:add_paddle(entities.right_paddle)
-
-  entities.ball = Ball(scene)
-end
-
--- Update the state of the world.
-function love.update(dt)
-  key_state:update()
-
-  for i, entity in pairs(entities) do
-    entity:update(dt, key_state)
-  end
-end
-
--- Draw ALL THE THINGS!
-function love.draw()
-  -- Draw the background.
-  local mid_x = love.graphics.getWidth() / 2
-  love.graphics.line(mid_x, 0, mid_x, love.graphics.getHeight())
-
-  -- Draw the moving stuff.
-  for i, entity in pairs(entities) do
-    entity:draw()
-  end
+  Gamestate.registerEvents()
+  Gamestate.switch(Ingame)
 end
